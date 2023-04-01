3ee37f2ee2bde7f8660cc67c6ed0725f7258f964
Dan Green <danngreen1@gmail.com>
2023-03-31 21:41:35 -0700

    Add NOR Flash pins to bootloader board conf

--- bootloader/brainboard_p3_conf.hh
+++ bootloader/brainboard_p3_conf.hh
@@ -3,7 +3,7 @@
 #include "drivers/i2c_conf.hh"
 #include "drivers/leds.hh"
 
-namespace BrainBoard_mp1_p3
+namespace Board
 {
 using RedLED = Led<GPIO::G, PinNum::_12, LedActive::High>;
 using GreenLED = RedLED;
@@ -18,6 +18,12 @@ constexpr uint32_t ConsoleUART = USART6_BASE;
 constexpr PinConf UartRX{GPIO::C, PinNum::_7, PinAF::AF_7};
 constexpr PinConf UartTX{GPIO::C, PinNum::_6, PinAF::AF_7};
 
+namespace NORFlash
+{
+constexpr PinConf d2{GPIO::F, PinNum::_7, PinAF::AF_9};
+constexpr PinConf d3{GPIO::F, PinNum::_6, PinAF::AF_9};
+} // namespace NORFlash
+
 namespace PMIC
 {
 constexpr bool HasSTPMIC = false;
@@ -28,4 +34,4 @@ constexpr uint32_t HSE_Clock_Hz = 24000000;
 constexpr uint32_t MPU_MHz = 800;
 constexpr auto ClockType = SystemClocks::HSEClockSource::Resonator;
 
-} // namespace BrainBoard_mp1_p3
+} // namespace Board
