7d4c11c373c651fc8573c3c4810240672abcb8d6
Dan Green <danngreen1@gmail.com>
2023-03-31 21:19:44 -0700

    Add board conf for bootloader for brainboard_p3

---
+++ bootloader/brainboard_p3_conf.hh
@@ -0,0 +1,31 @@
+#pragma once
+#include "drivers/clocks.hh"
+#include "drivers/i2c_conf.hh"
+#include "drivers/leds.hh"
+
+namespace BrainBoard_mp1_p3
+{
+using RedLED = Led<GPIO::G, PinNum::_12, LedActive::High>;
+using GreenLED = RedLED;
+using RedLED2 = RedLED;
+using GreenLED2 = GreenLED;
+using OrangeLED = RedLED2;
+using BlueLED = GreenLED;
+
+constexpr PinConf DFUpin{GPIO::E, PinNum::_11};
+
+constexpr uint32_t ConsoleUART = USART6_BASE;
+constexpr PinConf UartRX{GPIO::C, PinNum::_7, PinAF::AF_7};
+constexpr PinConf UartTX{GPIO::C, PinNum::_6, PinAF::AF_7};
+
+namespace PMIC
+{
+constexpr bool HasSTPMIC = false;
+constexpr I2C_Config I2C_config{};
+} // namespace PMIC
+
+constexpr uint32_t HSE_Clock_Hz = 24000000;
+constexpr uint32_t MPU_MHz = 800;
+constexpr auto ClockType = SystemClocks::HSEClockSource::Resonator;
+
+} // namespace BrainBoard_mp1_p3
